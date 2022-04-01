a971473e4b87d1b64b0fc9fb340e5e55111006cd
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-04-01 17:49:39 -0500

    refactor do-while loops into functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,64 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
-let alphaLowerSet = "abcdefghijklmnopqrstuvwxyz";
-let alphaUpperSet = alphaLowerSet.toLocaleUpperCase();
-let numberSet = "0123456789"
-let specialSet = "~`!@#$%^&*()_-+={}[]|\\;:\"'<,>.?/"
-let finalPassword = "";
-let passwordSet = "";
-
-function chooseCriteria() {
-  let alphaLowerChoice = false;
-  let alphaUpperChoice = false;
-  let numberChoice = false;
-  let specialChoice = false;
-  let choiceAlertPunc = ".";
-  do {
-    alert("Please select at least one set" + choiceAlertPunc);
-    alphaLowerChoice = confirm("Include lowercase letters?");
-    alphaUpperChoice = confirm("Include uppercase letters?");
-    numberChoice = confirm("Include numbers?");
-    specialChoice = confirm("Include special characters?");
-    choiceAlertPunc = "!"
-  }
-  while (!alphaLowerChoice && !alphaUpperChoice && !numberChoice && !specialChoice);
-  lengthChoice = prompt("How many characters should the password contain? Enter a number between 8 and 128.");
-  let lengthInvalid = false;
-  do {
-    lengthInvalid = false;
-    if (!Number(lengthChoice)) {
-      lengthInvalid = true;
-      lengthChoice = prompt("Length must be a number!");
-    }
-    else if (!Number.isInteger(Number(lengthChoice))) {
-      lengthInvalid = true;
-      lengthChoice = prompt("Length must be a positive integer!");
-    }
-    else if (lengthChoice < 8 || lengthChoice > 128) {
-      lengthInvalid = true;
-      lengthChoice = prompt("The length must be between 8 and 128!");
-    }
-  }
-  while (lengthInvalid);
-  console.log("lower:" + alphaLowerChoice + "\nupper:" + alphaUpperChoice + "\nnumber:" + numberChoice + "\nspecial:" + specialChoice);
-  if (alphaLowerChoice) {
-    passwordSet += alphaLowerSet;
-  }
-  if (alphaUpperChoice) {
-    passwordSet += alphaUpperSet;
-  }
-  if (numberChoice) {
-    passwordSet += numberSet;
-  }
-  if (specialChoice) {
-    passwordSet += specialSet;
-  }
-}
+let finalSet;
+let finalLength;
+let finalPassword;
 
 function generatePassword() {
-  chooseCriteria();
-  finalPassword = "";
-  for (let i = 0; i < lengthChoice; i++) {
-    randomNumber = Math.random() * passwordSet.length;
+  finalLength = generateLength();
+  finalSet = generateSet();
+  finalPassword = ""
+  for (let i = 0; i < finalLength; i++) {
+    randomNumber = Math.random() * finalSet.length;
     roundedNumber = Math.floor(randomNumber);
-    finalPassword = finalPassword + passwordSet[roundedNumber];
+    finalPassword += finalSet[roundedNumber];
   }
-  passwordSet = "";
+  finalSet = "";
   return finalPassword;
 }
-
+function generateLength() {
+  choiceLength = prompt("How many characters should the password contain? Enter a number between 8 and 128.");
+  let lengthValid = true;
+  if (!Number(choiceLength)) {
+    lengthValid = false;
+    choiceLength = alert("Length must be a number!");
+  }
+  else if (!Number.isInteger(Number(choiceLength))) {
+    lengthValid = false;
+    choiceLength = alert("Length must be a positive integer!");
+  }
+  else if (choiceLength < 8 || choiceLength > 128) {
+    lengthValid = false;
+    choiceLength = alert("The length must be between 8 and 128.");
+  }
+  if (!lengthValid) {
+    generateLength();
+  }
+  return choiceLength;
+}
+function generateSet() {
+  let alphaLowerSet = "abcdefghijklmnopqrstuvwxyz";
+  let alphaUpperSet = alphaLowerSet.toLocaleUpperCase();
+  let numberSet = "0123456789";
+  let specialSet = "~`!@#$%^&*()_-+={}[]|\\;:\"'<,>.?/";
+  choiceSet = "";
+  if (confirm("Include lowercase letters?")) {
+    choiceSet += alphaLowerSet;
+  }
+  if (confirm("Include uppercase letters?")) {
+    choiceSet += alphaUpperSet;
+  }
+  if (confirm("Include numbers?")) {
+    choiceSet += numberSet;
+  }
+  if (confirm("Include special characters?")) {
+    choiceSet += specialSet;
+  }
+  if (choiceSet === "") {
+    generateLength();
+  }
+  return choiceSet;
+}
 
 // Write password to the #password input
 function writePassword() {
